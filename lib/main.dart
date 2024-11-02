a6f703702fb3a78393181d668faba765be33e668
aswincs37 <aswincs37@gmail.com>
2024-11-02 19:01:31 +0530

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:talkloop/bottomnavbar.dart';
 import 'package:talkloop/provider/auth_provider.dart';
@@ -13,6 +14,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
+ await dotenv.load();
   runApp(const MyApp());
 }
 
